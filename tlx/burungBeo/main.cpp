4e013440ac51fc786dbe08567b19551001af8222
Syahril Fitrawan Abadi <syahrilfitrawanabadi@gmail.com>
2024-04-07 00:27:35 +0800

    add jawaban coding

---
+++ tlx/burungBeo/main.cpp
@@ -0,0 +1,11 @@
+// Fitra
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string burung_beo;
+    getline(cin, burung_beo);
+    cout << burung_beo;
+    return 0;
+}
---
+++ tlx/luasSegitiga/fitra.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int A, T;
+    float luas;
+    cin >> A >> T;
+    luas = 0.5 * A * T;
+    printf("%.2lf", luas);
+    return 0;
+}
